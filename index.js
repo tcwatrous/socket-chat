31183756baddb60ac85afced6159235ae92f05ac
Tim Watrous <twatrous@gmail.com>
2015-02-25 11:35:50 -0800

    io.sockets

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.sockets.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log('emitted ' + msg);
